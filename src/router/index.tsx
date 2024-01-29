4a355ecd4834b183e2a4f89505883456e3fad1cc
takenoko1395 <102368644+takenoko1395@users.noreply.github.com>
2024-01-29 22:04:04 +0900

    Add router (#3)
    * Vite導入
    
    * 不要なimport 削除
    
    * linter

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,23 @@
-import { createBrowserRouter } from "react-router-dom";
-import Button from "../pages/Button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../pages/app/App";
+import { MyButton } from "../pages/MyButton";
+import { ErrorFallback } from "../components/ErrorFallback";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorBoundaryComponent from '../pages/ErrorExample/error';
 
-const router = createBrowserRouter([
-  {
-    path: '/button',
-    element: (<Button title="I'm a Button"/>)
-  },
-  {
-    path: '/app',
-    element: (<App/>)
-  }
-])
-
-export default router
+export const AsterRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/app" element={<App />} />
+        <Route path="/button" element={
+          <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => console.log('Error boundary reset!')}>
+            <MyButton title="I will throw an error on click" />
+          </ErrorBoundary>
+          }
+        />
+        <Route path="/" element={<ErrorBoundaryComponent />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
