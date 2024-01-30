0196c9de3d7c255baf61b7c5e63edc034a193348
takenoko1395 <102368644+takenoko1395@users.noreply.github.com>
2024-01-30 19:22:39 +0900

    Linter (#4)
    * Linter対応
    
    * Update index.tsx

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,39 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@stylistic/recommended-extends',
   ],
-   parserOptions: {
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@stylistic'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@stylistic/jsx-one-expression-per-line': 'off', // <p>Hello, {name}</p> みたいなコードを許可する
+    '@stylistic/brace-style': [
+      'error',
+      '1tbs', // } else { 書き方を要求する
+    ],
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      // https://github.com/orgs/react-hook-form/discussions/9325#discussioncomment-4060566
+      // https://typescript-eslint.io/rules/no-misused-promises/#checksvoidreturn
+      { checksVoidReturn: {
+        arguments: false,
+        attributes: false,
+      } },
+    ],
+  },
+  parserOptions: {
     ecmaVersion: 'latest',
-     sourceType: 'module',
-     project: ['./tsconfig.json', './tsconfig.node.json'],
-     tsconfigRootDir: __dirname
-   }
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
 }
--- src/pages/MyButton/index.tsx
+++ src/pages/MyButton/index.tsx
@@ -1,35 +1,34 @@
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Button } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 type Props = {
-  title: string;
+  title: string
 }
 
 export const MyButton = ({ title }: Props) => {
   const onClick = () => {
-
     try {
       // ここで何かエラーが発生する可能性がある処理
       if (title === 'I will throw an error on click') {
-        throw new Error('An error occurred in MyButton on click!');
+        throw new Error('An error occurred in MyButton on click!')
       }
       // 通常のクリック処理
-      console.log('Button clicked!');
+      console.log('Button clicked!')
     } catch (error) {
       // エラー処理
-      console.error(error);
+      console.error(error)
       // ここでエラー処理を行う
     }
   }
-  
+
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Material-UI Button Example</h1>
-        <Link to="/App">
-          <Button onClick={onClick}>
-            {title}
-          </Button>
-        </Link>
+      <Link to="/App">
+        <Button onClick={onClick}>
+          {title}
+        </Button>
+      </Link>
     </div>
-  );
+  )
 }
